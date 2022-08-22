b760ec57f1e78a3c464ce446387616a28a9cf050
Claudio Guarisco <claudio.guarisco@netcom.it>
2022-08-22 17:55:29 +0200

    Let's begin.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # JS13K_Death
+Game for JS13K 2022 with theme: Death
---
+++ g.js
@@ -0,0 +1,175 @@
+// < >
+
+//global variables
+var canvas;
+var canvasW;
+var canvasH;
+var ctx;
+var dragging=false;
+var mousex=-100;
+var mousey=-100;
+
+//setup
+canvas = document.getElementById("g");
+ctx = canvas.getContext("2d");
+canvasW=canvas.width  = 1280;//window.innerWidth;
+canvasH=canvas.height = 800;//window.innerHeight;
+
+//controls
+canvas.addEventListener("mousemove",mossoMouse);
+canvas.addEventListener("mousedown",cliccatoMouse);
+canvas.addEventListener("mouseup",rilasciatoMouse);
+
+setInterval(run, 33);
+
+//draw a single object
+function draw(obj)
+{
+    ctx.fillStyle=obj.color;
+    if(obj.disabled)
+        ctx.globalAlpha=0.2;
+    if(obj.type=="cursor")
+    {
+        ctx.strokeStyle = "#000";
+        ctx.beginPath();
+        ctx.moveTo(obj.x, obj.y);
+        ctx.lineTo(obj.x, obj.y+17);
+        ctx.lineTo(obj.x+4, obj.y+15);
+        ctx.lineTo(obj.x+6, obj.y+20);
+        ctx.lineTo(obj.x+8, obj.y+20);
+        ctx.lineTo(obj.x+6, obj.y+15);
+        ctx.lineTo(obj.x+10, obj.y+13);
+        ctx.closePath();
+        ctx.stroke();
+        ctx.fill();
+    }
+    if(obj.type=="start")
+    {
+        ctx.font = "150px sans-serif";
+        ctx.fillText("🗺",obj.x,obj.y+100);
+    }
+    if(obj.type=="end")
+    {
+        ctx.font = "100px sans-serif";
+        ctx.fillText("🏁",obj.x,obj.y+100);
+    }
+    ctx.globalAlpha=1;
+}
+//main loop that draw the screen
+function run()
+{
+    ctx.clearRect(0, 0, canvasW, canvasH);
+    ctx.fillStyle="#000";
+    ctx.fillRect(0,0,canvasW,canvasH);
+    //border
+    ctx.fillStyle="#FFF";
+    ctx.fillRect(0,0,canvasW,1);
+    ctx.fillRect(0,canvasH-1,canvasW,1);
+    ctx.fillRect(0,0,1,canvasH);
+    ctx.fillRect(canvasW-1,0,1,canvasH);
+
+    var start=new Object()
+    start.type="start";
+    start.x=canvasW-200;
+    start.y=canvasH-100;
+    draw(start)
+    var end=new Object()
+    end.type="end";
+    end.x=50;
+    end.y=50;
+    end.disabled=true;
+    draw(end);
+
+    //DEBUG
+    var tmp=new Object()
+    tmp.type="end";
+    tmp.x=50;
+    tmp.y=50;
+    tmp.disabled=true;
+    draw(tmp);
+}
+/*#############
+    Funzioni Utili
+##############*/
+function rand(da, a)
+{
+    if(da>a) return rand(a,da);
+    a=a+1;
+    return Math.floor(Math.random()*(a-da)+da);
+}
+function distanceFrom(ax,ay,bx,by)
+{
+    return Math.sqrt((ax-bx)*(ax-bx)+(ay-by)*(ay-by));
+}
+//uindows
+function cliccatoMouse(evt)
+{
+    dragging=true;
+    var rect = canvas.getBoundingClientRect();
+    mousex=(evt.clientX-rect.left)/(rect.right-rect.left)*canvasW;
+    mousey=(evt.clientY-rect.top)/(rect.bottom-rect.top)*canvasH;
+}
+function mossoMouse(evt)
+{
+    var rect = canvas.getBoundingClientRect();
+    mousex=(evt.clientX-rect.left)/(rect.right-rect.left)*canvasW;
+    mousey=(evt.clientY-rect.top)/(rect.bottom-rect.top)*canvasH;
+}
+function rilasciatoMouse(evt)
+{
+    dragging=false;    
+}
+window.AutoScaler = function(element, initialWidth, initialHeight, skewAllowance){
+    var self = this;
+    
+    this.viewportWidth  = 0;
+    this.viewportHeight = 0;
+    
+    if (typeof element === "string")
+        element = document.getElementById(element);
+    
+    this.element = element;
+    this.gameAspect = initialWidth/initialHeight;
+    this.skewAllowance = skewAllowance || 0;
+    
+    this.checkRescale = function() {
+        if (window.innerWidth == self.viewportWidth && 
+            window.innerHeight == self.viewportHeight) return;
+        
+        var w = window.innerWidth;
+        var h = window.innerHeight;
+        
+        var windowAspect = w/h;
+        var targetW = 0;
+        var targetH = 0;
+        
+        targetW = w;
+        targetH = h;
+        
+        if (Math.abs(windowAspect - self.gameAspect) > self.skewAllowance) {
+            if (windowAspect < self.gameAspect)
+                targetH = w / self.gameAspect;
+            else
+                targetW = h * self.gameAspect;
+        }
+        
+        self.element.style.width  = targetW + "px";
+        self.element.style.height = targetH + "px";
+    
+        self.element.style.marginLeft = ((w - targetW)/2) + "px";
+        self.element.style.marginTop  = ((h - targetH)/2) + "px";
+    
+        self.viewportWidth  = w;
+        self.viewportHeight = h;
+        
+    }
+    
+    // Ensure our element is going to behave:
+    self.element.style.display = 'block';
+    self.element.style.margin  = '0';
+    self.element.style.padding = '0';
+    
+    // Add event listeners and timer based rescale checks:
+    window.addEventListener('resize', this.checkRescale);
+    rescalercheck=setInterval(this.checkRescale, 1500);
+};
